4fbb7a3639376a621264d7baa0d37c0ec4d8e147
ruemilley <ruemilley>
2024-07-06 17:22:16 -0700

    finalized some code for preserving objects across scenes so that if you drop them in different places they stay where they are. also tweaked player jump and changed the main scene to not the initial level so it will be easier to implement changes later

---
+++ Scripts/primary_aisle.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+func _ready():
+	#spawn item in global item array
+	if Global.primary_aisle_items.size() > 0:
+		for i in Global.primary_aisle_items:
+			Global.spawn_item(i)
+	
+	
+	#set player position if reentering scene
+	if Global.last_main_position != Vector2(0,0): #make sure it's not the default value
+		$Player.position = Global.last_main_position
+		$Player.position.y = 0.0 #put you on the ground in case you were jumping when you went through the door
+
