86ee52793d05889393790d9c975d7ba415625ebf
ruemilley <ruemilly@gmail.com>
2024-08-19 10:50:24 -0700

    adding step sound effect

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,17 +2,26 @@ extends CharacterBody2D
 
 
 const SPEED := 600.0
+const RUN_SPEED := 1000.0
 const JUMP_VELOCITY := -600.0
 const FALL_GRAVITY := 1800
-const INTERACT_TEXT := "E to "
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction
 
+#interactions
+const INTERACT_TEXT := "E to "
 @onready var all_interactions = []
 @onready var interaction_label = $InteractionComponents/InteractLabel
+
+#inventory
 @onready var inventory_ui = $Inventory
+
+#other misc variables
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var walk_sound = $WalkSound
+
 
 
 func  _ready():
@@ -22,11 +31,10 @@ func  _ready():
 
 
 func _physics_process(delta):
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += get_gravity(velocity) * delta
-		$AnimatedSprite2D.play("jump")
+		animated_sprite.play("jump")
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor() and Global.dialogue_active == false:
@@ -36,30 +44,35 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY / 4
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	direction = Input.get_axis("walk_left", "walk_right")
 	if direction and Global.dialogue_active == false:
 		velocity.x = direction * SPEED
-		$AnimatedSprite2D.play("walk")
+		animated_sprite.play("walk")
+		if Input.is_action_pressed("run") and is_on_floor():
+			velocity.x = direction * RUN_SPEED
+		if Input.is_action_pressed("jump") and Input.is_action_pressed("run"):
+			velocity.x = direction * RUN_SPEED
+		if is_on_floor() and !walk_sound.playing:
+			walk_sound.pitch_scale = randf_range(.9, 1.1)
+			walk_sound.play()
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		walk_sound.stop()
 	
 	if velocity.x < 0 and Global.dialogue_active == false:
-		$AnimatedSprite2D.flip_h = true
+		animated_sprite.flip_h = true
 		Global.player_direction = true
 		
 	if velocity.x > 0 and Global.dialogue_active == false:
-		$AnimatedSprite2D.flip_h = false
-
+		animated_sprite.flip_h = false
 		Global.player_direction = false
 	
 	if velocity.x == 0 and velocity.y == 0:
-		$AnimatedSprite2D.play("idle")
-		
+		animated_sprite.play("idle")
 		
 	#keep player from moving when screen is paused
 	if get_tree().paused == true:
-		$AnimatedSprite2D.play("idle")
+		animated_sprite.play("idle")
 		velocity.x = 0
 		velocity.y = 0
 	
